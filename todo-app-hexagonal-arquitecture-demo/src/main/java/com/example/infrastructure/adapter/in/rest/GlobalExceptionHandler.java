f75dbd9d6d1b7d8a05dd911d709e28eb13eaaf81
vrma <vrmachado@gmail.com>
2026-09-13 17:24:48 +0200

    RestControllerAdvise en curso

---
+++ todo-app-hexagonal-arquitecture-demo/src/main/java/com/example/infrastructure/adapter/in/rest/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.example.infrastructure.adapter.in.rest;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
